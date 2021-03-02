3db76de513934a7efd9fbe51c9ba5d4cbbc21c17
Bernardo Marques <bernardohmarques@hotmail.com>
2021-03-02 16:04:36 -0300

    chore: create initial routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
+
+import MainPage from "./pages/main";
+import Login from "./pages/login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline>
+        
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route>
+              <MainPage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </CssBaseline>
+    </>
   );
 }
 
---
+++ src/pages/main/index.jsx
@@ -0,0 +1,15 @@
+import { Route, Switch } from "react-router-dom";
+
+const MainPage = () => {
+  return (
+    <>
+      <h1>Main</h1>
+
+      <Switch>
+          
+      </Switch>
+    </>
+  );
+};
+
+export default MainPage;
