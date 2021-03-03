2ba32617c689172b5b2aff36913fd5bfae2f0745
Bernardo Marques <bernardohmarques@hotmail.com>
2021-03-03 04:07:21 -0300

    chore: add firebase to login with github

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,12 +1,26 @@
 import { Grid } from "@material-ui/core";
+import firebase from 'firebase'
+
 import { ReactComponent as Logo } from "./logo.svg";
 import { Container, GitHubButton } from "./style";
+
+let firebaseConfig = {
+  apiKey: "AIzaSyDYC9VfzWkwSxR9Ge3vScQCGVw3Opr16J8",
+  authDomain: "reactzzaria-e4844.firebaseapp.com",
+  projectId: "reactzzaria-e4844",
+  storageBucket: "reactzzaria-e4844.appspot.com",
+  messagingSenderId: "180684174259",
+  appId: "1:180684174259:web:0decd9f8a46a918b5311c6",
+  measurementId: "G-12NR86ZRD8",
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 const Login = () => {
   return (
     <Container>
       <Grid container justify="center" spacing={5}>
-        <Grid item >
+        <Grid item>
           <Logo />
         </Grid>
 
