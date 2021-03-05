2e51464f10dfe7a53b8857e388ab10efac461550
Bernardo Marques <bernardohmarques@hotmail.com>
2021-03-05 19:17:46 -0300

    style: pizza cards

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -6,11 +6,19 @@ import {
   MenuItem,
   Grid,
   withStyles,
-  Paper,
 } from "@material-ui/core";
 import { AccountCircle } from "@material-ui/icons";
 import { useContext, useState } from "react";
-import { Content, Logo, LogoContainer, Toolbar } from "./style";
+import {
+  PaperPizza,
+  Content,
+  Divider,
+  Logo,
+  LogoContainer,
+  Toolbar,
+  Pizza,
+  PizzaText,
+} from "./style";
 import { AuthContext } from "../../contexts/auth";
 
 const MainPage = () => {
@@ -52,18 +60,28 @@ const MainPage = () => {
 
       <Content>
         <Grid container direction="column" alignItems="center">
-          <Typography variant="h3">O que vai ser hoje, {userName}?</Typography>
+          <Typography variant="h3" gutterBottom>
+            O que vai ser hoje, {userName}?
+          </Typography>
 
-          <Typography variant="h4">Escolha o tamanho da pizza:</Typography>
+          <Typography variant="h4" gutterBottom>
+            Escolha o tamanho da pizza:
+          </Typography>
 
           <Grid container spacing={2}>
             {pizzaSizes.map((pizza) => (
               <Grid item key={pizza.id} xs={4}>
-                <Paper style={{padding: 20}}>
-                  <div>{pizza.size}cm</div>
-                  <Typography>{pizza.name}</Typography>
-                  <Typography>{pizza.slices} fatias, {pizza.flavours} sabores</Typography>
-                </Paper>
+                <PaperPizza>
+                  <Pizza>
+                    <PizzaText>{pizza.size}cm</PizzaText>
+                  </Pizza>
+
+                  <Divider />
+                  <Typography variant="h5">{pizza.name}</Typography>
+                  <Typography>
+                    {pizza.slices} fatias, {pizza.flavours} sabores
+                  </Typography>
+                </PaperPizza>
               </Grid>
             ))}
           </Grid>
--- src/pages/main/style.js
+++ src/pages/main/style.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import { ReactComponent as MainLogo } from "../../images/logo.svg";
-import { Toolbar as MaterialToolbar } from "@material-ui/core";
+import {
+  Toolbar as MaterialToolbar,
+  Divider as MaterialDivider,
+  Paper,
+  Typography,
+} from "@material-ui/core";
 
 export const LogoContainer = styled.div`
   flex-grow: 1;
@@ -26,5 +31,59 @@ export const Toolbar = styled(MaterialToolbar)`
 `;
 
 export const Content = styled.main`
-    padding: 20px;
+  padding: 20px;
+`;
+
+export const Divider = styled(MaterialDivider)`
+  margin: 20px 0;
+  width: 100%;
+`;
+
+export const PaperPizza = styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Pizza = styled.div`
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  width: 200px;
+  position: relative;
+
+  &::before,
+  &::after {
+    background: #ccc;
+    content: "";
+    position: absolute;
+    transform: rotate(45deg);
+  }
+
+  &::before {
+    height: 1px;
+    width: 160px;
+  }
+
+  &::after {
+    height: 160px;
+    width: 1px;
+  }
+`;
+
+export const PizzaText = styled(Typography).attrs({
+    variant: 'h5'
+})`
+ height: 80px;
+ width: 80px;
+ border-radius: 50%;
+ background: #fff;
+ display: flex;
+ justify-content: center;
+ align-items: center; 
+ position: relative;
+ z-index: 1;
 `
