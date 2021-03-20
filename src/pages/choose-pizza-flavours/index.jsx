bfb02ec6a4e4f3d2f564017a5ae75e1ddf2fef14
Bernardo Marques <bernardohmarques@hotmail.com>
2021-03-20 00:28:25 -0300

    chore: verification for pizza flavours limit

--- src/pages/choose-pizza-flavours/index.jsx
+++ src/pages/choose-pizza-flavours/index.jsx
@@ -1,4 +1,5 @@
 import { Card, Grid, Typography } from "@material-ui/core";
+import { useState } from "react";
 import { Redirect } from "react-router";
 import Divider from "../../components/divider";
 import PizzasGrid from "../../components/pizzas-grid/";
@@ -10,12 +11,31 @@ import { Img, Label } from "./style";
 
 
 const ChoosePizzaFlavours = ({ location }) => {
+  const [checkboxes, setCheckboxes] = useState(() => ({}))
+
+
   if (!location.state) {
     return <Redirect to={HOME} />;
   }
 
   const howMuchFlavours = location.state.flavours;
   const { id } = location.state;
+
+  const checkboxesChecked = (checkboxes) => {
+    return Object.values(checkboxes).filter(Boolean)
+  }
+  
+  const handleChangeCheckbox = (pizzaId) => (e) => {
+    if (checkboxesChecked(checkboxes).length === howMuchFlavours && e.target.checked === true) {
+      return
+    }
+    setCheckboxes((checkboxes) => {
+      return {
+        ...checkboxes,
+        [pizzaId]: e.target.checked
+      }
+    })
+  }
 
   return (
     <>
@@ -31,7 +51,11 @@ const ChoosePizzaFlavours = ({ location }) => {
           <Grid item key={pizza.id} xs={12} sm={4}>
             <Card>
               <Label>
-                  <input type='checkbox' />
+                  <input
+                   type='checkbox'
+                   checked={!!checkboxes[pizza.id]}
+                   onChange={handleChangeCheckbox(pizza.id)}
+                   />
                 <Img src={pizza.image} alt={pizza.name} />
 
                 <Divider />
